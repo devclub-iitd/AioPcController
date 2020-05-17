96aadf4ca3a73f9eaad26d5bb825f1d62d879644
pavasgdb <pavasgdb@gmail.com>
2020-05-17 22:37:31 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/aio_pc_controller/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aio_pc_controller
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/foundation.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+void main() async {
+  // modify with your true address/port
+  Socket sock = await Socket.connect('192.168.43.3', 4444);
+  runApp(MyApp(sock));
+}
+
+class MyApp extends StatelessWidget {
+  Socket socket;
+
+  MyApp(Socket s) {
+    this.socket = s;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'AIO pc Controller';
+    return MaterialApp(
+      title: title,
+      home: MyHomePage(
+        title: title,
+        channel: socket,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final Socket channel;
+
+  MyHomePage({Key key, @required this.title, @required this.channel})
+      : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Form(
+              child: TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            StreamBuilder(
+              stream: widget.channel,
+              builder: (context, snapshot) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: Text(snapshot.hasData
+                      ? '${String.fromCharCodes(snapshot.data)}'
+                      : ''),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: Icon(Icons.send),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      widget.channel.write(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.channel.close();
+    super.dispose();
+  }
+}
+
