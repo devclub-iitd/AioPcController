194126de253dba7aa6b40d30d48c9639704a9015
Sarang Chaudhari <cs1180381@iitd.ac.in>
2020-05-27 18:15:33 +0530

    Blank msg not displayed in driver, Display host disconnected, Code cleanup

--- driver.py
+++ driver.py
@@ -71,40 +71,44 @@ def main():
 	qr = QRCode()
 	qr.add_data(serverIP)
 	qr.print_ascii()
-	print ("Socket is listening...")
 
 	while True: 
-		
-		c, addr = s.accept()
-		
-		print ('Got connection from', addr)
+		print ("Socket is listening...")
 
-		while True:
-			message = c.recv(256).decode("utf-8"); #message comes in byte array so change it to string first
+		try:
+			c, addr = s.accept()
+			
+			print ('Got connection from', addr)
 
-			message = message.split("%") #use & to split tokens, and % to split messages.
-			
-			for msg in message:
-				msg = msg.split("&")
-				print("DEBUG: ", msg)
-				if(msg[0] == 'wasd'):
-    					wasd(msg[1], msg[2])
-				elif(msg[0] == 'tilt'):
-					if(len(msg) == 2):
-						if(msg[1] == '0'):
-							global button
-							button = '$'
-					if(len(msg) < 3):
-						continue
-					elif(msg[2] == ''):
-						continue
-					elif msg[1] == '+':
-						tilt(True, float(msg[2]))
-					elif msg[1] == '-':
-						tilt(False, float(msg[2]))
-			c.send(bytes('Thank you for connecting', "utf-8"))
-			
-		c.close()
+			while True:
+				message = c.recv(256).decode("utf-8"); #message comes in byte array so change it to string first
+
+				message = message.split("%") #use & to split tokens, and % to split messages.
+				
+				for msg in message:
+					if msg != '':
+						msg = msg.split("&")
+						print("DEBUG: ", msg)
+						if(msg[0] == 'wasd'):
+							wasd(msg[1], msg[2])
+						elif(msg[0] == 'tilt'):
+							if(len(msg) == 2):
+								if(msg[1] == '0'):
+									global button
+									button = '$'
+							if(len(msg) < 3):
+								continue
+							elif(msg[2] == ''):
+								continue
+							elif msg[1] == '+':
+								tilt(True, float(msg[2]))
+							elif msg[1] == '-':
+								tilt(False, float(msg[2]))
+				c.send(bytes('Thank you for connecting', "utf-8"))
+				
+			c.close()
+		except:
+			print('Host Disconnected! Please Connect Again...\n\n')
 
 def wasd(type, msg):
 	if(type == 'down'):
@@ -113,14 +117,6 @@ def wasd(type, msg):
 	else:
 		# pyautogui.keyUp(msg)
 		keyboard.release(msg)
-
-def press(duty_ratio,button):
-	j = 15
-	for i in range (j-6):
-		pyautogui.keyDown(button)
-		time.sleep((0.15/(j))*duty_ratio)
-		pyautogui.keyUp(button)
-
 
 def tilt(message,value):
 	global button
