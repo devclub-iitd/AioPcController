4a82e3394ab3e29250f26a9373cc50d6c609553f
Harsh Wardhan <hwardhan337@gmail.com>
2020-05-28 00:11:02 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -22,14 +22,14 @@ class myThread (threading.Thread):
 	def run(self):
 		global sub
 		while(True):
-			if(button == '$'):
-				time.sleep(0.05)
+			if button == '$'or duty_ratio==0 :
+				time.sleep(0.005)
 				continue
 			sub = button
 			keyboard.press(sub)
-			time.sleep(0.05*duty_ratio)
+			time.sleep(0.005*duty_ratio)
 			keyboard.release(sub)
-			time.sleep((0.05)*(1-duty_ratio))
+			time.sleep((0.005)*(1-duty_ratio))
 
 
 thread1 = myThread(1, "Thread-1", 1)
