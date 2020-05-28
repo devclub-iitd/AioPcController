ce8f7fbc3e1532108697296e7e9d681883c5f337
Ritvik Gupta <ritvikgupta199@gmail.com>
2020-05-28 19:11:47 +0530

    Removed TapUp and driver.py bugs
    Bug where KeyUp was not called when the pointer is slightly moved.
    Python code did not exit when port was already in use.

--- driver.py
+++ driver.py
@@ -1,11 +1,12 @@
 import socket
-# import pyautogui
+import pyautogui
 import time
 from pynput.keyboard import Key, Controller
 import subprocess
 import threading
 from qrcode import QRCode
-# pyautogui.PAUSE = 0.01
+import os
+pyautogui.PAUSE = 0.01
 keyboard = Controller()
 button = '$'
 duty_ratio = 0
@@ -23,13 +24,13 @@ class myThread (threading.Thread):
 		global sub
 		while(True):
 			if button == '$'or duty_ratio==0 :
-				time.sleep(0.005)
+				time.sleep(0.05)
 				continue
 			sub = button
 			keyboard.press(sub)
-			time.sleep(0.005*duty_ratio)
+			time.sleep(0.05*duty_ratio)
 			keyboard.release(sub)
-			time.sleep((0.005)*(1-duty_ratio))
+			time.sleep((0.05)*(1-duty_ratio))
 
 
 thread1 = myThread(1, "Thread-1", 1)
@@ -54,7 +55,7 @@ def main():
 		except OSError:
 			print("Port " + str(port) + " is already in use. Please use the following command:")
 			print("lsof -n -i4TCP:" + str(port) +" | grep LISTEN")
-			exit()
+			os._exit(os.EX_OK)
 		except:
 			print ("Unexpected error while connecting to port")
      
@@ -74,48 +75,46 @@ def main():
 	while True: 
 		print ("Socket is listening...")
 
-		try:
-			c, addr = s.accept()
+		c, addr = s.accept()
+		
+		print ('Got connection from', addr)
+
+		while True:
+			message = c.recv(256).decode("utf-8"); #message comes in byte array so change it to string first
+
+			message = message.split("%") #use & to split tokens, and % to split messages.
 			
-			print ('Got connection from', addr)
-
-			while True:
-				message = c.recv(256).decode("utf-8"); #message comes in byte array so change it to string first
-
-				message = message.split("%") #use & to split tokens, and % to split messages.
-				
-				for msg in message:
-					if msg != '':
-						msg = msg.split("&")
-						print("DEBUG: ", msg)
-						if(msg[0] == 'wasd'):
-							wasd(msg[1], msg[2])
-						elif(msg[0] == 'tilt'):
-							if(len(msg) == 2):
-								if(msg[1] == '0'):
-									global button
-									button = '$'
-							if(len(msg) < 3):
-								continue
-							elif(msg[2] == ''):
-								continue
-							elif msg[1] == '+':
-								tilt(True, float(msg[2]))
-							elif msg[1] == '-':
-								tilt(False, float(msg[2]))
-				c.send(bytes('Thank you for connecting', "utf-8"))
-				
-			c.close()
-		except:
-			print('Host Disconnected! Please Connect Again...\n\n')
+			for msg in message:
+				if msg != '':
+					msg = msg.split("&")
+					print("DEBUG: ", msg)
+					if(msg[0] == 'wasd'):
+						wasd(msg[1], msg[2])
+					elif(msg[0] == 'tilt'):
+						if(len(msg) == 2):
+							if(msg[1] == '0'):
+								global button
+								button = '$'
+						if(len(msg) < 3):
+							continue
+						elif(msg[2] == ''):
+							continue
+						elif msg[1] == '+':
+							tilt(True, float(msg[2]))
+						elif msg[1] == '-':
+							tilt(False, float(msg[2]))
+			c.send(bytes('Thank you for connecting', "utf-8"))
+			
+		c.close()
+		
 
 def wasd(type, msg):
 	if(type == 'down'):
-		# pyautogui.keyDown(msg)
-		keyboard.press(msg)
+		pyautogui.keyDown(msg)
+		# keyboard.press(msg)
 	else:
-		# pyautogui.keyUp(msg)
-		keyboard.release(msg)
+		pyautogui.keyUp(msg)
+		# keyboard.release(msg)
 
 def tilt(message,value):
 	global button
